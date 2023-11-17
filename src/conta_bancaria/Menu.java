c8a3195005338ab9f7ad4441c9eeb22f6337bf16
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-11-17 02:06:25 -0300

    Menu

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,10 +1,98 @@
 package conta_bancaria;
 
+import java.util.Scanner;
+
+import conta_bancaria.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
 
+		int opcao;
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			opcao = leia.nextInt();
+
+			if (opcao == 9) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+				case 1:
+					System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+				
+                    break;
+				case 2:
+					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
+
+					break;
+				case 3:
+					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+
+					break;
+				case 4:
+					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
+
+					break;
+				case 5:
+					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+
+					break;
+				case 6:
+					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+
+					break;
+				case 7:
+					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+
+					break;
+				case 8:
+					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+
+					break;
+				default:
+					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+					break;
+			}
+		}
+
+	}
+
+	public static void sobre() {
+		System.out.println(Cores.TEXT_GREEN);
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
+		System.out.println(Cores.TEXT_RESET);
 	}
 
 }
