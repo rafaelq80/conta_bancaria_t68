74aaf04963d4b550eba1449d0106af03c3f51459
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-11-22 11:50:51 -0300

    Interface + CRUD

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -2,7 +2,7 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
-import conta_bancaria.model.Conta;
+import conta_bancaria.controller.ContaController;
 import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.model.ContaPoupanca;
 import conta_bancaria.util.Cores;
@@ -12,36 +12,18 @@ public class Menu {
 	public static void main(String[] args) {
 		
 		Scanner leia = new Scanner(System.in);
-
-		int opcao;
 		
-		/*Criar Objeto da Classe Conta para testes*/
+		ContaController contas = new ContaController();
 		
-		Conta c1 = new Conta(1, 123, 1, "Victória Moraes", 100000.00f);
-		c1.visualizar();
-		System.out.println("Exibir o Saldo: " + c1.getSaldo());
-		c1.setSaldo(200000.00f);
-		c1.visualizar();
-		c1.sacar(210000.00f);
-		c1.visualizar();
-		c1.depositar(5000.00f);
-		c1.visualizar();
-
-		/*Criar Objeto da Classe ContaCorrente para testes*/
-		ContaCorrente cc1 = new ContaCorrente(2, 456, 1, "Felipe", 100000.00f, 2000.00f);
-		cc1.visualizar();
-		cc1.sacar(101000.00f);
-		cc1.visualizar();
-		cc1.depositar(2000.00f);
-		cc1.visualizar();
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
 		
-		/*Criar Objeto da Classe ContaPoupanca para testes*/
-		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Vitor Nascimento", 3000.00f, 12);
-		cp1.visualizar();
-		cp1.sacar(1000.00f);
-		cp1.visualizar();
-		cp1.depositar(2000.00f);
-		cp1.visualizar();
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000.00f, 100.00f);
+		contas.cadastrar(cc1);
+		
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 123, 2, "Maria da Silva", 1000.00f, 12);
+		contas.cadastrar(cp1);
 		
 		while (true) {
 
@@ -79,10 +61,36 @@ public class Menu {
 				case 1:
 					System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
 				
+					System.out.println("Digite o número da Agência: ");
+					agencia = leia.nextInt();
+					
+					System.out.println("Digite o nome do Titular: ");
+					leia.skip("\\R");
+					titular = leia.nextLine();
+					
+					System.out.println("Digite o tipo da Conta (1 - CC ou 2 - CP): ");
+					tipo = leia.nextInt();
+					
+					System.out.println("Digite o Saldo da conta: ");
+					saldo = leia.nextFloat();
+					
+					switch(tipo) {
+						case 1 -> {
+							System.out.println("Digite o limite da conta: ");
+							limite = leia.nextFloat();
+							contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+						}
+						case 2 -> {
+							System.out.println("Digite o dia do aniversário da conta: ");
+							aniversario = leia.nextInt();
+							contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+						}
+					}
+					
                     break;
 				case 2:
 					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-
+						contas.listarTodas();
 					break;
 				case 3:
 					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
---
+++ src/conta_bancaria/controller/ContaController.java
@@ -0,0 +1,73 @@
+package conta_bancaria.controller;
+
+import java.util.ArrayList;
+
+import conta_bancaria.model.Conta;
+import conta_bancaria.repository.ContaRepository;
+
+public class ContaController implements ContaRepository{
+
+	// Criar a Collection
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+	
+	// Variável para receber o numero da Conta
+	int numero = 0;
+	
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarTodas() {
+		for(var conta : listaContas) {
+			conta.visualizar();
+		}
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		listaContas.add(conta);
+		System.out.println("A Conta numero: " + conta.getNumero() + " foi criada com Sucesso!");
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/* Métodos Auxiliares */
+	
+	public int gerarNumero() {
+		return ++ numero;
+	}
+
+}
